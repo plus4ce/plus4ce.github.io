e20502892a9c45476200af7ffa9cadb165bb22fe
Plus_sPS <plus_sps@163.com>
2023-06-08 19:33:30 +0800

    NEW!Version

---
+++ js/window-js.js
@@ -0,0 +1,18 @@
+$(function () {
+
+});
+function gotoIndex() {
+    $("#content").attr("src", "index.html")
+
+}
+function gotoBlog() {
+    $("#content").attr("src", "Blog.html")
+
+}
+function gotoJoinUs() {
+    $("#content").attr("src", "JoinUs.html")
+
+}
+function gotoNKStudio() {
+    $("#content").attr("src", "NKStudio.html")
+}
